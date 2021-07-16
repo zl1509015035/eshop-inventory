b686eaeb79808adeb2774e8713e8a4823004945f
zl1509015035 <1509015035@qq.com>
2021-07-16 08:58:13 +0800

    忽略文件

--- redis-demo1-40/src/main/java/com/example/redisdemo140/demo/JedisTest43.java
+++ redis-demo1-40/src/main/java/com/example/redisdemo140/demo/JedisTest43.java
@@ -7,8 +7,14 @@ public class JedisTest43 {
         Jedis jedis = new Jedis("192.168.1.10", 6379);
         jedis.auth("redis-pass");
 
+        jedis.del("operation_log_2021_01_01");
 
+        for (int i = 0; i < 10; i++) {
+            jedis.append("operation_log_2021_01_01","今天的第"+(i+1)+"条操作日志\n");
+        }
 
+        String operationLog = jedis.get("operation_log_2021_01_01");
+        System.out.println("今天所有的操作日志:\n"+operationLog);
 
     }
 }
