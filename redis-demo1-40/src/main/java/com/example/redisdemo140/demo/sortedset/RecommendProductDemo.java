40600702bb22d8706066ce35065b2569eb20a519
zl1509015035 <1509015035@qq.com>
2021-07-21 21:44:03 +0800

    set 购买过此商品的人推荐商品

---
+++ redis-demo1-40/src/main/java/com/example/redisdemo140/demo/sortedset/RecommendProductDemo.java
@@ -0,0 +1,68 @@
+package com.example.redisdemo140.demo.sortedset;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Tuple;
+
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * 案例实战：购买此商品的顾客也同时购买
+ * <p>
+ * zremrangebyscore
+ * zunionstore
+ * zinterstore
+ */
+public class RecommendProductDemo {
+
+    private Jedis jedis = new Jedis("192.168.1.10", 6379);
+
+    public void flushDB() {
+        jedis.flushDB();
+    }
+
+    public static void main(String[] args) {
+        RecommendProductDemo demo = new RecommendProductDemo();
+        demo.flushDB();
+
+        int productId = 1;
+        for (int i = 0; i < 20; i++) {
+            demo.continuePurchase(productId, i+2);
+        }
+
+        for(int i = 0;i<3;i++){
+            demo.continuePurchase(productId,i+2);
+        }
+
+        Set<Tuple> recommendProducts = demo.getRecommendProducts(productId);
+        Iterator<Tuple> iterator = recommendProducts.iterator();
+        System.out.println("购买过此商品的人，还购买过以下商品:");
+        while(iterator.hasNext()){
+            Tuple next = iterator.next();
+            System.out.println(next.getElement()+" "+next.getScore());
+        }
+
+    }
+
+    /**
+     * 继续购买商品
+     *
+     * @param productId
+     * @param otherProductId
+     */
+    public void continuePurchase(long productId, long otherProductId) {
+        jedis.zincrby("continue_purchase_products::" + productId, 1, String.valueOf(otherProductId));
+    }
+
+    /**
+     * 推荐其他人购买过的商品
+     *
+     * @param productId
+     * @return
+     */
+    public Set<Tuple> getRecommendProducts(long productId) {
+        return jedis.zrevrangeWithScores("continue_purchase_products::" + productId, 0, 2);
+    }
+
+
+}
