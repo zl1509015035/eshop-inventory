f48d88ae485d19d9d03ba431b6529d000b6ac4a0
zl1509015035 <1509015035@qq.com>
2021-07-23 07:48:21 +0800

    判断是否是本人释放的锁

--- redis-demo1-40/src/main/java/com/example/redisdemo140/demo/expire/TimeoutDistributedLockDemo.java
+++ redis-demo1-40/src/main/java/com/example/redisdemo140/demo/expire/TimeoutDistributedLockDemo.java
@@ -1,6 +1,9 @@
 package com.example.redisdemo140.demo.expire;
 
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Pipeline;
+
+import java.io.IOException;
 
 /**
  * 案例实战：超时自动释放锁
@@ -17,10 +20,13 @@ public class TimeoutDistributedLockDemo {
         demo.flushDB();
         demo.lock("test_lock", "testvalue", 10);
 
-        Thread.sleep(12*1000);
+        Thread.sleep(12 * 1000);
 
         Boolean result = demo.lock("test_lock", "testvalue", 10);
         System.out.println("第二次加锁结果:" + (result ? "是" : "否"));
+
+        System.out.println("不是你本人能否释放锁:"+(demo.unlock("test_lock", "xxx")?"能":"不能"));
+        System.out.println("是你本人能否释放锁:"+(demo.unlock("test_lock", "testvalue")?"能":"不能"));
 
 
     }
@@ -31,7 +37,42 @@ public class TimeoutDistributedLockDemo {
         return result > 0;
     }
 
-    public void unlock(String key) {
-        jedis.del(key);
+    /**
+     * 释放锁，需要判断是否是你自己加的锁，才可以释放锁
+     *
+     * @param key
+     */
+    public Boolean unlock(String key, String vlaue) {
+        //在pipeline取出来
+        String currentValue = jedis.get(key);
+
+        Pipeline pipeline = jedis.pipelined();
+
+        try {
+            pipeline.watch(key);
+            //为空则已经释放掉了
+            if (currentValue == null || currentValue.equals("") || currentValue.equals("null")) {
+                return true;
+            }
+
+            if (currentValue.equals(vlaue)) {
+                pipeline.multi();
+                pipeline.del(key);
+                pipeline.exec();
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            try {
+                pipeline.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
